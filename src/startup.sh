ffbc9052b30cc97ba4f145dce8e4d29f76b3ef03
TylerJulian <tyler.james.julian@ou.edu>
2021-11-27 04:47:10 -0600

    test

--- src/startup.sh
+++ src/startup.sh
@@ -1,6 +1,7 @@
 sudo apt install docker.io
-sudo systemctl enable --now docker
+#sudo systemctl enable --now docker
 
+sudo /etc/init.d/docker start 
 sudo usermod -aG docker $USER
 
 sudo apt install docker-compose
